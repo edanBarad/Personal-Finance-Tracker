d6a36fb6db0885b5fbedcf8d82ffd5baf8a3ecc3
Edan Barad <edan@barad.org>
2025-03-03 21:00:16 +0200

    3.3.2025

---
+++ src/Transaction.java
@@ -0,0 +1,51 @@
+public class Transaction {
+    private double amount;
+    private String category;
+    private String date;
+    private String type;
+
+    public Transaction(double amount, String category, String date, String type){
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString(){
+        return "Amount: " + this.amount + "\nCategory: " + this.category + "\nDate: " + this.date + "\nType: " + this.type;
+    }
+
+}
